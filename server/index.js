46e37582781e603a6a06e269590bea071eae2759
Vasanth Santosh <vasanth.santosh0@gmail.com>
2025-08-27 12:50:12 +1000

    Added connection to mongodb database

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import connectToDB from './config/db.js';
+import dotenv from 'dotenv';
+
+import { User } from './models/users.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(express.json()) // might not need
+
+connectToDB() 
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+
+const user1 = new User({
+    username: "a",
+    password: "testerpass",
+    email: "a@helloworld.com"
+});
+
+const savedUser = await user1.save();
+console.log('User created:', savedUser);
