f7c9e54436668ebf225247ade5f9e74de4773ff5
Phoebe Zhang <zphoebe@outlook.com>
2025-10-15 00:19:26 +1100

    Fix permissions dropdown

--- server/src/app.js
+++ server/src/app.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: ['http://localhost:3000'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
     'Origin',
     'X-Requested-With',
